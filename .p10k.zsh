6af28c6b940d7224756db427827fef48e229fec6
Theo Patron <theo.patron.ext@axa.com>
2025-09-04 14:00:17 +0000

    fix os background color for tmux

--- .p10k.zsh
+++ .p10k.zsh
@@ -191,7 +191,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND='black' #232
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
